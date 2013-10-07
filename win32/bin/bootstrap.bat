882a89d10c96ccf3f5478011b538240569e0b21a
Piaoger <piaoger@yahoo.com>
2013-10-07 15:01:14 +0800

    Add node.exe download back

--- win32/bin/bootstrap.bat
+++ win32/bin/bootstrap.bat
@@ -4,4 +4,5 @@ set THIS_FOLDER=%THIS_FOLDER_TAIL:~0,-1%
 md node
 
 curl -SsL --output cacert.pem  http://curl.haxx.se/ca/cacert.pem
+curl -SsL --output node/node.exe  http://nodejs.org/dist/v0.10.20/x64/node.exe
 call "node/node.exe" bootstrap.js
