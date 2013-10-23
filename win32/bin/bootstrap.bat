7cf5090d5b2fbc5fd5ee81bdad7bc61ddd349600
Piaoger <piaoger@yahoo.com>
2013-10-23 22:27:42 +0800

    update

--- win32/bin/bootstrap.bat
+++ win32/bin/bootstrap.bat
@@ -1,8 +1,7 @@
 set THIS_FOLDER_TAIL=%~dp0
 set THIS_FOLDER=%THIS_FOLDER_TAIL:~0,-1%
 
+:: Download node.js
 md node
-
-curl -SsL --output cacert.pem  http://curl.haxx.se/ca/cacert.pem
 curl -SsL --output node/node.exe  http://nodejs.org/dist/v0.10.20/x64/node.exe
 call "node/node.exe" bootstrap.js
